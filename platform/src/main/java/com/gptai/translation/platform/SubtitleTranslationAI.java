2aa97e03a3775482e51e0f12e83493b9f80c5372
xiao.wenpeng <ja_xwp@yeah.net>
2025-03-10 15:42:05 +0900

    chore: 初始化maven模块

--- platform/src/main/java/com/gptai/translation/platform/SubtitleTranslationAI.java
+++ platform/src/main/java/com/gptai/translation/platform/SubtitleTranslationAI.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SubtitleTranslationAIApplication {
+public class SubtitleTranslationAI {
 
     public static void main(String[] args) {
-        SpringApplication.run(SubtitleTranslationAIApplication.class, args);
+        SpringApplication.run(SubtitleTranslationAI.class, args);
     }
 }
